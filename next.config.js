0111ec67bc22a97b53d24ed4e6f6ef5e3ecf8175
santhossiva2002 <santhossiva2002@gmail.com>
2025-05-21 00:57:48 +0530

    4th

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',                       
-  basePath: '/portfolio-nextjs',          
-  assetPrefix: '/portfolio-nextjs/',      
+
   eslint: {
-    ignoreDuringBuilds: true,             
+    ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },          
-  trailingSlash: true,                   
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
