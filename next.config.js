9e540315f2150cbd86fce2e2f3464313a8b31d2f
santhossiva2002 <santhossiva2002@gmail.com>
2025-05-21 00:43:20 +0530

    second commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
-  trailingSlash: true, // Optional but helps with GitHub Pages
-};
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    output: 'export',
+    basePath: '/portfolio-nextjs',
+    assetPrefix: '/portfolio-nextjs/',
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    images: { unoptimized: true },
+    trailingSlash: true, // Optional but helps with GitHub Pages
+  };
 
-module.exports = nextConfig;
+  module.exports = nextConfig;
